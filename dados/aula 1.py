ccf58a19824cc75930747598686e25faab67ab1b
ioritsu <49842341+ioritsu@users.noreply.github.com>
2019-05-13 15:56:41 -0300

    Conditions to win
    player 1 wins? or player 2?

--- dados/aula 1.py
+++ dados/aula 1.py
@@ -7,16 +7,21 @@ print(" Jogo de Dados")
 
 dados1 = randint(1, 6)
 dados2 = randint(1, 6)
-
-print("Dado 1:", dados1)
-print("Dado 2:", dados2)
-
 jogador1 = dados1 + dados2
 
 dados3 = randint(1, 6)
 dados4 = randint(1, 6)
+jogador2 = dados3 + dados4
 
+print("Dado 1:", dados1)
+print("Dado 2:", dados2)
 print("Dado 3:", dados3)
 print("Dado 4:", dados4)
 
-jogador2 = dados3 + dados4
+print("Jogador 1:", jogador1)
+print("Jogador 2:", jogador2)
+
+if jogador1>jogador2:
+    print("jogador 1 é foda")
+else:
+    print("jogador 2 deu a volta por cima")
