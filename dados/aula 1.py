36e1c454c078bc1604fb626d42b8a41319c4624f
ioritsu <49842341+ioritsu@users.noreply.github.com>
2019-05-13 15:37:18 -0300

    Add files via upload

--- dados/aula 1.py
+++ dados/aula 1.py
@@ -1,13 +1,22 @@
+# Programa dos Dados
+# Carlos Eduardo G. Corrêa 
+
 from random import randint
 
 print(" Jogo de Dados")
 
 dados1 = randint(1, 6)
 dados2 = randint(1, 6)
-dados3 = randint(1, 6)
-dados4 = randint(1, 6)
 
 print("Dado 1:", dados1)
 print("Dado 2:", dados2)
+
+jogador1 = dados1 + dados2
+
+dados3 = randint(1, 6)
+dados4 = randint(1, 6)
+
 print("Dado 3:", dados3)
 print("Dado 4:", dados4)
+
+jogador2 = dados3 + dados4
